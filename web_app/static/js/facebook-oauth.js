ffd01619907032db0c0c0c9a1aeee8aec28fbbce
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-12-07 10:27:28 -0800

    refactored FB API & added better status check

--- web_app/static/js/facebook-oauth.js
+++ web_app/static/js/facebook-oauth.js
@@ -1,34 +1,24 @@
-// Constsants
-const nowTime = new Date();
-
-// Globals
-var userName;
-var userEmail;
-var userId;
-var userPhoto;
-
-// used for syncing modals with images
-const index = {'count': 0};
-
+// FB API initializer
 window.fbAsyncInit = function () {
   FB.init({
-    appId: '131891487494882', // our FB app
+    appId: '131891487494882',
     channelUrl: 'https://www.cecinestpasun.site/',
-    status: true, // check login status
+    status: true,
     cookie: true,
     xfbml: true
   });
 
-  FB.Event.subscribe('auth.authResponseChange', function (response) {
-    if (response.status === 'connected') {
-      document.getElementById('message').innerHTML = '<br>Connected to Facebook';
-    } else if (response.status === 'not_authorized') {
-      document.getElementById('message').innerHTML = '<br>Failed to Connect';
-    } else {
-      document.getElementById('message').innerHTML = '<br>Logged Out';
-    }
-  });
+  $(document).trigger('fbload');
 };
+
+// Load the SDK asynchronously
+(function (d) {
+  var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+  if (d.getElementById(id)) { return; }
+  js = d.createElement('script'); js.id = id; js.async = true;
+  js.src = '//connect.facebook.net/en_US/all.js';
+  ref.parentNode.insertBefore(js, ref);
+}(document));
 
 function Login () {
   FB.login(function (response) {
@@ -39,36 +29,41 @@ function Login () {
     }
   }, {scope: 'email,user_likes,user_videos'});
 }
+
+function buildFBHTML (userName, userId, userEmail, imgHTML) {
+  let userBar = [
+    '<b>Name</b> : ' + userName + '<br>',
+    '<b>id: </b>' + userId + '<br>',
+    '<b>email: </b>' + userEmail + '<br>',
+    '<input type="button" value="Logout" onclick="Logout();"/>',
+    '<div id="fb_profile_image">' + imgHTML + '</div>'
+  ];
+  document.getElementById('status').innerHTML = userBar.join('');
+}
+
+function buildFBError(cod, message) {
+  let userBar = [
+    '<strong>ERROR login failure</strong><br>',
+    '<strong>' + code + '</strong><br>',
+    '<p>' + message + '</p>'
+  ];
+  document.getElementById('status').innerHTML = userBar.join('');
+}
+
 function getUserInfo () {
   FB.api('/me?fields=name,email,id', function (response) {
     if (response.error == undefined) {
-      userName = response.name;
-      userId = response.id;
-      userEmail = response.email
-      getPhoto()
-      var imgHtml = "<img src='" + userPhoto + "'/>";
-      let userBar = [
-        '<b>Name</b> : ' + userName + '<br>',
-        '<b>id: </b>' + userId + '<br>',
-        '<b>email: </b>' + userEmail + '<br>',
-        '<input type="button" value="Logout" onclick="Logout();"/>',
-        '<div id="fb_profile_image">' + imgHtml + '</div>'
-      ];
-      document.getElementById('status').innerHTML = userBar.join('');
+      let userName = response.name;
+      let userId = response.id;
+      let userEmail = response.email
+      FB.api('/me/picture?type=normal', function (response2) {
+	let userPhoto = response2.data.url;
+	let imgHTML = "<img src='" + userPhoto + "'/>";
+	buildFBHTML(userName, userId, userEmail, imgHTML);
+      });
     } else {
-      let userBar = [
-        '<strong>ERROR login failure</strong><br>',
-        '<strong>' + response.code + '</strong><br>',
-        '<p>' + response.error.message + '</p>'
-      ];
-      document.getElementById('status').innerHTML = userBar.join('');
+      buildFBError(response.code, response.error.message);
     }
-  });
-}
-
-function getPhoto () {
-  FB.api('/me/picture?type=normal', function (response) {
-    userPhoto = response.data.url;
   });
 }
 
@@ -76,11 +71,27 @@ function Logout () {
   FB.logout(function () { document.location.reload(); });
 }
 
-// Load the SDK asynchronously
-(function (d) {
-  var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-  if (d.getElementById(id)) { return; }
-  js = d.createElement('script'); js.id = id; js.async = true;
-  js.src = '//connect.facebook.net/en_US/all.js';
-  ref.parentNode.insertBefore(js, ref);
-}(document));
+function checkFacebookStatus () {
+
+  FB.getLoginStatus(function(res){
+    if ( res.status == "unknown" ){
+      document.getElementById('message').innerHTML = '<br>Logged Out';
+    } else if ( res.status === 'connected' ) {
+      getUserInfo();
+      document.getElementById('message').innerHTML = '<br>Connected to Facebook';
+    }
+  });
+  FB.Event.subscribe('auth.authResponseChange', function (response) {
+    if (response.status === 'connected') {
+      document.getElementById('message').innerHTML = '<br>Connected to Facebook';
+    } else if (response.status === 'not_authorized') {
+      document.getElementById('message').innerHTML = '<br>Failed to Connect';
+    } else {
+      document.getElementById('message').innerHTML = '<br>Logged Out';
+    }
+  });
+}
+
+$(document).ready(function () {
+  $(document).on('fbload', checkFacebookStatus);
+});
